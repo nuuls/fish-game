04fa7de8310862a0f6fe13cacfdf929883ced4d7
nuuls <nilsv21@gmail.com>
2022-12-14 11:43:15 +0100

    actually looks cool now

--- src/game.rs
+++ src/game.rs
@@ -5,16 +5,48 @@ use crate::level;
 pub struct Game {
     level: level::Level,
     render_buffer: Vec<Triangle>,
-    triangles: Vec<Triangle>,
+    game_items: Vec<ShitItem>,
+}
+
+pub trait GameItem {
+    fn into_triangle(&self) -> Triangle;
 }
 
 pub type Triangle = [f32; 9];
+
+struct ShitItem {
+    triangle: Triangle,
+    moving: [f32; 9],
+}
+
+impl GameItem for ShitItem {
+    fn into_triangle(&self) -> Triangle {
+        self.triangle
+    }
+}
+
+impl ShitItem {
+    fn update(&mut self) {
+        let tri = &mut self.triangle;
+        for n in 0..tri.len() {
+            let hit_edge = tri[n] >= 1.0 || tri[n] <= -1.0;
+
+            let movement = match hit_edge {
+                true => -self.moving[n],
+                false => self.moving[n],
+            };
+
+            tri[n] = tri[n] + movement;
+            self.moving[n] = movement;
+        }
+    }
+}
 
 impl Game {
     pub fn new() -> Game {
         Game {
             render_buffer: vec![],
-            triangles: random_triangles(5),
+            game_items: random_shit_items(5),
             level: level::Level::load_from_svg("xd"),
         }
     }
@@ -28,12 +60,11 @@ impl Game {
 
         // data
 
-        for tri in &mut self.triangles {
-            for n in 0..tri.len() {
-                tri[n] = tri[n] + ((random() * 2.0 - 1.0) * 0.01).clamp(-1.0, 1.0) as f32;
-            }
-            self.render_buffer.push(tri.clone());
+        for item in &mut self.game_items {
+            item.update();
+            self.render_buffer.push(item.into_triangle());
         }
+
         self.render_buffer.clone()
     }
 }
@@ -46,6 +77,21 @@ fn random_triangles(n: usize) -> Vec<Triangle> {
                 t[i] = (random() * 2.0 - 1.0) as f32;
             }
             t
+        })
+        .collect()
+}
+
+fn random_shit_items(n: usize) -> Vec<ShitItem> {
+    (0..n)
+        .map(|_| {
+            let mut t: Triangle = [0.0; 9];
+            for i in 0..9 {
+                t[i] = (random() * 2.0 - 1.0) as f32;
+            }
+            ShitItem {
+                triangle: t,
+                moving: [0.001; 9],
+            }
         })
         .collect()
 }
