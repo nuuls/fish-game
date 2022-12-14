79c08f9360f12364314f3156374f1f3564197473
nuuls <nilsv21@gmail.com>
2022-12-14 10:44:54 +0100

    little bit less seizure

--- src/game.rs
+++ src/game.rs
@@ -4,6 +4,7 @@ use crate::level;
 
 pub struct Game {
     level: level::Level,
+    render_buffer: Vec<Triangle>,
     triangles: Vec<Triangle>,
 }
 
@@ -12,24 +13,28 @@ pub type Triangle = [f32; 9];
 impl Game {
     pub fn new() -> Game {
         Game {
-            triangles: vec![],
+            render_buffer: vec![],
+            triangles: random_triangles(5),
             level: level::Level::load_from_svg("xd"),
         }
     }
 
     pub fn next_frame(&mut self) -> Vec<Triangle> {
-        self.triangles.clear();
+        self.render_buffer.clear();
         let level_triangles = self.level.triangles();
         for tri in level_triangles {
-            self.triangles.push(tri)
+            self.render_buffer.push(tri)
         }
 
         // data
-        for tri in random_triangles(5) {
-            self.triangles.push(tri);
-        }
 
-        self.triangles.clone()
+        for tri in &mut self.triangles {
+            for n in 0..tri.len() {
+                tri[n] = tri[n] + ((random() * 2.0 - 1.0) * 0.01).clamp(-1.0, 1.0) as f32;
+            }
+            self.render_buffer.push(tri.clone());
+        }
+        self.render_buffer.clone()
     }
 }
 
